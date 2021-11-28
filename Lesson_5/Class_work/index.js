c31e3155c4811538101d17f9e9f8f3e2d83074da
Adis <Spartanez509@gmail.com>
2021-11-29 01:32:13 +0300

    лекция функции

--- Lesson_5/Class_work/index.js
+++ Lesson_5/Class_work/index.js
@@ -1,71 +1,85 @@
 // function helloWorld() {
-//     ('helloWorld')
+//   console.log("Hello World");
 // }
+
 // helloWorld();
 
 // const isUserAuth = true;
 // function checkAccess() {
 //   if (isUserAuth) {
-//     ("Доступ есть");
+//     console.log("Доступ есть");
 //   } else {
-//     ("Доступв нет");
+//     console.log("Доступа нет");
 //   }
 // }
 
 // checkAccess();
 
-// function writeMassage() {
-//     if (isUserAuth) {
-//         ("Привет пользователь");
-//       } else {
-//         ("Привет гость");
-//       }
-//     }
+// function writeMessage() {
+//   if (isUserAuth) {
+//     console.log("Привет Пользователь");
+//   } else {
+//     console.log("Привет Гость");
+//   }
+// }
+// writeMessage();
 
-//     writeMassage();
-// function sum(a, b= 0) {
-//     (b);
-//     (a + b);
+// function sum(a, b = 0) {
+//   console.log(b);
+//   console.log(a + b);
 // }
 // sum(1, 4);
-// sum(16);
+// sum(15);
 
-// let name = "Alex";
+// function getName(name) {
+//   console.log("до return");
+//   return name;
+//   console.log("после return");
+// }
+// // getName("Ivan");
+// const userName = getName("Max");
+// console.log(userName);
 
-// function getName(useName) {
-//   return useName;
+// function getName(name) {
+//   return [1, 2, 3, 45, 6];
 // }
 
+// const userName = getName("Max");
+// console.log(userName);
+
+// function getName(userName) {
+//   return userName;
+// }
+
+// let name = "ALex";
+
 // const nameUserOne = getName(name);
-// (nameUserOne);
 
 // name = "Ivan";
-// name = "Ivan";
 
-// const nameUserTwo = getName(name);
-// (nameUserTwo);
+// console.log(nameUserOne);
 
-// let name ="Alex"
+// let name = "ALex";
 
-// (name, "1")
+// console.log(name, "1");
 
 // function sayHi(userName) {
-//     userName ="Petr"
-//     (userName, "function");
-
+//   userName = "Petr";
+//   name = "Max";
+//   console.log(userName, "function");
 // }
 
 // sayHi(name);
 
-// (name, "2");
+// console.log(name, "2");
 
 // let a = 0;
-// let b = 1;
+// let b = 0;
 
 // function sum() {
-//   a + 1;
-//   b + 1;
-//   (a + b);
+//   a += 1;
+//   b += 1;
+//   console.log(a + b);
 // }
 // sum();
 // sum();
@@ -75,118 +89,205 @@
 // function sum() {
 //   const a = 0;
 //   const b = 1;
-//   (a + b);
+//   console.log(a + b);
 // }
 // sum();
 // sum();
 // sum();
 // sum();
 
-// let userName = "Dima";
+//################### Практика ########################
 
-// function hiUser (name) {
-//     if (name) {
-//         (`Привет ${name}`);
-//       } else {
-//         ("Привет гость");
-//       }
-//     }
+// Написать функцию, которая будет принимать имя пользователя, и выводить строку с приветствием данного пользователя, если имени пользователя нет, выводить приветствие гостя.
 
-// hiUser("Dima");
+// function userName(name) {
+//   name = "Dima";
+//   if (name) {
+//     console.log("Привет пользователь", `${name}`);
+//   } else {
+//     console.log("Привет гость");
+//   }
+// }
+// userName();
+
+//Написать функцию, которая в качестве первого параметра будет принимать численное значение, а в качестве второго параметра будет принимать степень, в которую надо возвести первый аргумент. По умолчанию, второй аргумент единица.
 
 // function exp(a, b = 1) {
-//   (a ** b);
+//   console.log(a ** b);
 // }
 
 // exp(2, 2);
 // exp(2, 8);
-// exp(2,18);
+// exp(20);
 
-// let showText = function (text)  {
-// 	alert (text);
+//Написать функцию, которая будет принимать в качестве входного параметра массив любых, целых чисел, и будет возвращать среднее арифметическое значение данного массива*
+
+// function getValue(arr) {
+//   let sum = 0;
+//   for (let i = 0; i < arr.length; i += 1) {
+//     sum += arr[i];
+//   }
+//   return sum / arr.length;
 // }
-// (showText); // код функции
 
-// const sayName = function(name) {
-//     return name;
+// console.log(getValue([1, 23, 6]));
 
-// }
+//####################################################################################
 
-// (sayName ('Alex'));
+// const sayName = function (name) {
+//   return name;
+// };
+
+// const copy = sayName;
+// console.log(sayName("Alex"));
+
+// console.log(copy("Ivan"));
 
 // const getName = function () {
-//   ("Alex");
+//   console.log("Alex");
 // };
 
 // const getUserInfo = function (callBack) {
-//   ("User Info");
+//   console.log("USER INFO");
 //   callBack();
 // };
 
 // getUserInfo(getName);
+
 // test();
 
-// function test () {
-//     ('test');
+// function test() {
+//   console.log("test");
 // }
 
-// // test2(); нельзя обратиться до ее инициализации
-
-// const test2= function () {
-//     ("test2")
-// }
+// const test2 = function () {
+//   console.log("test2");
+// };
 
 // test2();
 
-// const myfirstArrowFunc = () => ("!!");
+// const myFirstArrowFunc = () => console.log("!!");
+// const test1 = (name) => {
+//   //func body
+//   return name;
+// };
 
-// const test2 = (name,age) => (name, age);
+// console.log(test1("ALex"));
 
-// test2 ("alex",19)
+// const getLog = () => console.log("lOG");
+// getLog();
 
 // // FD
 // function example1(param) {
-//     return param;
+//   return param;
 // }
-
 // example1(1);
-
 // //FE
-
 // const example2 = function (param) {
-//     return param;
-// }
+//   return param;
+// };
 // example2(2);
-
 // //AF.1
-
 // const example3 = (param) => param;
 // example3(3);
+// //AF.2
+// const example3 = (param) => {
+//   //big body
+//   return param;
+// };
+// example3(3);
 
-// задачи на стрелочные функции
+// const getString = (str, count) => {
+//   for (let i = 0; i < count; i += 1) {
+//     console.log(str);
+//   }
+// };
 
-// const getString = (str,count) => {
-//     for(let i = 0; i < count; i+= 1) {
-//         (str);
+// const isPositive = (letter) => {
+//   //   if (letter === "a" || letter === "y") {
+//   //     return true;
+//   //   } else {
+//   //     return false;
+//   //   }
+//   switch (letter) {
+//     case "a":
+//     case "y":
+//     case "i":
+//       console.log("true");
+//       break;
+//     default:
+//       console.log("false");
+//   }
+// };
+
+// isPositive("i");
+
+// Дано число. Сложите его цифры. Если сумма получилась более 9-ти, опять сложите его цифры.
+//И так, пока сумма не станет однозначным числом (9 и менее).
+
+// const evenNine = (num) => {
+//   num = `${num}`;
+//   let sum = 0;
+//   for (let i = 0; i < num.length; i += 1) {
+//     sum += +num[i];
+//   }
+
+//   if (sum > 9) {
+//     return evenNine(sum);
+//   } else {
+//     return sum;
+//   }
+// };
+
+// console.log(evenNine(2478));
+
+// Дан массив с числами. Выведите последовательно его элементы.
+
+// const arr = [2, 4, 3, 1];
+
+// const arrSort = (arr) => {
+//   const result = []; // создаем массив в который положим результат
+//   for (let j = 0; j < arr.length; j += 1) {
+//     // запусткаем внешний цикл, который будет запускать логику сортировки поэтапно для каждого элемента
+//     for (let i = 0; i < arr.length; i += 1) {
+//       // запускаем цикл который будет выполнять поэлементную проверку значений в массиве
+//       if (!i) {
+//         // присваиваем первый элемент в масиив результатов, этот элемент будет отправной точкой
+//         result[i] = arr[i];
+//         continue;
+//       }
+//       // для последующих элементов выполняем проверку на значение
+//       if (arr[i] > result[i - 1]) {
+//         // если текущий элемент больше предыдущего то добавляем его в массив результатов
+//         result[i] = arr[i];
+//       } else {
+//         // если текущий элемен меньше чем предыдущий, то помещаем его на место i-1, а предыдущий элемент на место i
+//         const a = result[i - 1];
+//         result[i - 1] = arr[i];
+//         result[i] = a;
+//       }
 //     }
-// } ;
-// getString("Ivan",3);
+//     // после каждой итерации первго цикла, производим перезапись входного массива, и запускаем сортировку повторно
+//     arr = result;
+//   }
+//   // после выполнения циклов выозвращаем результат
+//   return result;
+// };
 
-// getString("London",13);
+// console.log(arrSort(arr));
+
+// const isPalindrom = (str) => {
+//   let newStr = "";
+//   for (let i = 0; i < str.length; i += 1) {
+//     newStr += str[str.length - i - 1];
+//   }
+//   if (newStr === str) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+
+// console.log(isPalindrom("abgba"));
 
 
- const isPositive = (letter) => {   
-
-
-switch (letter) {
-  case 'y':
-  case 'u':
-  case 'e':
-  case 'o':
-   console.log ("true");
-   break;
-  default:
-    console.log("false");
-  }
- };
-isPositive('y');
